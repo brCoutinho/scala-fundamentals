2b3c03276b4551095db414f960a5a12e4f2b1e2d
Thiago Coutinho <thcoutinho1997@gmail.com>
2022-02-03 11:25:42 -0300

    Sets and Tuples

---
+++ src/aulas/Sets.scala
@@ -0,0 +1,11 @@
+package aulas
+
+object Sets {
+  def main(args: Array[String]): Unit = {
+
+    //Não mantém a ordem e é imutável
+    val numeros = Set(1,1,2,3,5,4,6)
+    println(numeros)
+  }
+
+}
---
+++ src/aulas/Tuples.scala
@@ -0,0 +1,9 @@
+package aulas
+
+object Tuples {
+  def main(args: Array[String]): Unit = {
+    val hostPort = (List("localhost", "AWS", "Oracle Cloud"), 80, 8080, 8080)
+    println(hostPort)
+
+  }
+}
