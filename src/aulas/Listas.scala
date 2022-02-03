50fac1074e4fa41055f1e69ee34f17ce41f35fe1
Thiago Coutinho <thcoutinho1997@gmail.com>
2022-02-03 10:52:00 -0300

    Listas

--- src/aulas/Listas.scala
+++ src/aulas/Listas.scala
@@ -2,8 +2,9 @@ package aulas
 
 object Listas {
   def main(args: Array[String]): Unit = {
-    val lista = List(1,2,3,4)
+    val lista = List(1,11,21,31)
 
+    //Head e Tail
     val listaHead = lista.head
     println("Head: " + listaHead)
 
@@ -12,9 +13,24 @@ object Listas {
 
     println(listaHead +: listaTail)
 
+    //Map e Filter
     val valores = List(10,9,8)
     println(valores.map(_ + 1 +" é um número"))
 
     println(lista.filter(_ %2 == 0))
+
+    //Map e FlatMap
+    val par = (x: Int) => List(x, x+1)
+    println(lista.flatMap(par))
+
+    println(lista.map(par))
+
+    val inteiros = List(1,2,3,4)
+    val char = List("a", "b", "c", "d")
+
+    println()
+    println("FlatMap")
+    val juncao = inteiros.flatMap(n => char.map(c => "" + c + n))
+    println(juncao)
   }
 }
